6e0ade67f0f35a43f032fd7e04a40c553ca725ce
teri0411 <goaheadman@naver.com>
2022-04-17 23:35:52 +0900

    discord webhook

--- discord-bot/.aws-sam/build/helloFromLambdaFunction/src/handlers/hello-from-lambda.js
+++ discord-bot/.aws-sam/build/helloFromLambdaFunction/src/handlers/hello-from-lambda.js
@@ -6,11 +6,26 @@ const axios = require("axios");
 exports.helloFromLambdaHandler = async (event) => {
     console.log(JSON.stringify(event))
     try{
-        const result = await axios.post("https://discord.com/api/webhooks/963673376562487306/g4syYfcN1742Zg7yWu2MO_gFCa--THJsrm1MZmRCf8AnEJigLjSfIy-5Gh43E9B0exMp");
+
+        const bitcoinResult = await axios.get("http://api.coindesk.com/v1/bpi/currentprice.json");
+        
+
+
+
+        const result = await axios.post("https://discord.com/api/webhooks/963673376562487306/g4syYfcN1742Zg7yWu2MO_gFCa--THJsrm1MZmRCf8AnEJigLjSfIy-5Gh43E9B0exMp",{
+            "content":bitcoinResult.data.bpi.USD.rate,
+            "embeds": [{
+                  "image": {
+                    "url": "https://cdn.dribbble.com/users/430985/screenshots/1762272/attachments/286970/Arsene-Lupin_2.png"
+                  }
+                }]
+              
+        });
+        
         console.info("디스코드 웹훅 성공");
     }
     catch(err){
-        console.err("웹훅 실패", err);
+        console.log("웹훅 실패", err);
     }
 
     const response = {
