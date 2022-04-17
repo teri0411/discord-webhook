178ce880bfe26039aa57c56b0b45839f31bdab1b
teri0411 <goaheadman@naver.com>
2022-04-18 00:16:21 +0900

    discord-webhook-bot

--- discord-bot/src/handlers/hello-from-lambda.js
+++ discord-bot/src/handlers/hello-from-lambda.js
@@ -15,7 +15,7 @@ exports.helloFromLambdaHandler = async (event) => {
         const result = await axios.post("https://discord.com/api/webhooks/963673376562487306/g4syYfcN1742Zg7yWu2MO_gFCa--THJsrm1MZmRCf8AnEJigLjSfIy-5Gh43E9B0exMp",{
             "username":"제가 1994년 LA다저스에 있었을때..",
             "avatar_url":"https://post-phinf.pstatic.net/MjAyMTAxMTNfMTI2/MDAxNjEwNTIzMDM0MzU2.2doMPH4L1wuZm9An07CT4u7NMtLkpap7dUowUeIeDwwg.29Dugauct2nQ6MfcRt0hB7pFj8dXGRPDijpJffWp4U0g.JPEG/images.jpg",
-            "content":bitcoinResult.data.bpi.USD.rate,
+            "content":"",
             "embeds": [{
                   "image": {
                     "url": "https://img2.daumcdn.net/thumb/R658x0.q70/?fname=https://t1.daumcdn.net/news/201905/11/mksports/20190511170600736cewm.jpg"
