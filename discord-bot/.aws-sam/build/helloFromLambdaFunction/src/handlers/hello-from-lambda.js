43a3a75f4da5fc322b314b7880b8c203a4e93f2a
teri0411 <goaheadman@naver.com>
2022-04-17 22:49:41 +0900

    discord webhook

--- discord-bot/.aws-sam/build/helloFromLambdaFunction/src/handlers/hello-from-lambda.js
+++ discord-bot/.aws-sam/build/helloFromLambdaFunction/src/handlers/hello-from-lambda.js
@@ -3,16 +3,19 @@
  */
 const axios = require("axios");
 
-exports.helloFromLambdaHandler = async () => {
-    // If you change this message, you will need to change hello-from-lambda.test.js
+exports.helloFromLambdaHandler = async (event) => {
+    console.log(JSON.stringify(event))
     try{
-        const result = await axios.post("https://discord.com/api/webhooks/963673376562487306/g4syYfcN1742Zg7yWu2MO_gFCa--THJsrm1MZmRCf8AnEJigLjSfIy-5Gh43E9B0exMp", {
-            "content":"Welcome rupin Wolrd!"
-        });
+        const result = await axios.post("https://discord.com/api/webhooks/963673376562487306/g4syYfcN1742Zg7yWu2MO_gFCa--THJsrm1MZmRCf8AnEJigLjSfIy-5Gh43E9B0exMp");
         console.info("디스코드 웹훅 성공");
     }
     catch(err){
         console.err("웹훅 실패", err);
+    }
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from lambda'),
     }
     const message = 'Hello from Lambda!';
 
